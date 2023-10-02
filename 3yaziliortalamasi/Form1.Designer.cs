981c433161c7e8c4ab8fbe9ac5d523bd0bed27f4
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-02 13:28:38 +0300

    Add files via upload

---
+++ 3yaziliortalamasi/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace _3yaziliortalamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNot1 = new System.Windows.Forms.Label();
+            this.lblNot2 = new System.Windows.Forms.Label();
+            this.lblNot3 = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.txtNot1 = new System.Windows.Forms.TextBox();
+            this.txtNot2 = new System.Windows.Forms.TextBox();
+            this.txtNot3 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNot1
+            // 
+            this.lblNot1.AutoSize = true;
+            this.lblNot1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNot1.Location = new System.Drawing.Point(46, 48);
+            this.lblNot1.Name = "lblNot1";
+            this.lblNot1.Size = new System.Drawing.Size(60, 25);
+            this.lblNot1.TabIndex = 0;
+            this.lblNot1.Text = "not1:";
+            // 
+            // lblNot2
+            // 
+            this.lblNot2.AutoSize = true;
+            this.lblNot2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNot2.Location = new System.Drawing.Point(46, 92);
+            this.lblNot2.Name = "lblNot2";
+            this.lblNot2.Size = new System.Drawing.Size(60, 25);
+            this.lblNot2.TabIndex = 1;
+            this.lblNot2.Text = "not2:";
+            // 
+            // lblNot3
+            // 
+            this.lblNot3.AutoSize = true;
+            this.lblNot3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNot3.Location = new System.Drawing.Point(46, 138);
+            this.lblNot3.Name = "lblNot3";
+            this.lblNot3.Size = new System.Drawing.Size(60, 25);
+            this.lblNot3.TabIndex = 2;
+            this.lblNot3.Text = "not3:";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOrtalama.Location = new System.Drawing.Point(46, 204);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(183, 25);
+            this.lblOrtalama.TabIndex = 3;
+            this.lblOrtalama.Text = "ortalamaniz: ____";
+            // 
+            // txtNot1
+            // 
+            this.txtNot1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNot1.Location = new System.Drawing.Point(112, 45);
+            this.txtNot1.Name = "txtNot1";
+            this.txtNot1.Size = new System.Drawing.Size(142, 31);
+            this.txtNot1.TabIndex = 4;
+            // 
+            // txtNot2
+            // 
+            this.txtNot2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNot2.Location = new System.Drawing.Point(112, 89);
+            this.txtNot2.Name = "txtNot2";
+            this.txtNot2.Size = new System.Drawing.Size(142, 31);
+            this.txtNot2.TabIndex = 5;
+            // 
+            // txtNot3
+            // 
+            this.txtNot3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNot3.Location = new System.Drawing.Point(112, 132);
+            this.txtNot3.Name = "txtNot3";
+            this.txtNot3.Size = new System.Drawing.Size(142, 31);
+            this.txtNot3.TabIndex = 6;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(85, 263);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(284, 46);
+            this.btnHesapla.TabIndex = 7;
+            this.btnHesapla.Text = "not hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtNot3);
+            this.Controls.Add(this.txtNot2);
+            this.Controls.Add(this.txtNot1);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.lblNot3);
+            this.Controls.Add(this.lblNot2);
+            this.Controls.Add(this.lblNot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNot1;
+        private System.Windows.Forms.Label lblNot2;
+        private System.Windows.Forms.Label lblNot3;
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.TextBox txtNot1;
+        private System.Windows.Forms.TextBox txtNot2;
+        private System.Windows.Forms.TextBox txtNot3;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ 3yaziliortalamasi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3yaziliortalamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int not1, not2, not3, sonuc;
+            not1 = Convert.ToInt32(txtNot1.Text);
+            not2 = Convert.ToInt32(txtNot2.Text);
+            not3 = Convert.ToInt32(txtNot3.Text);
+
+            sonuc = (not1 + not2 + not3) / 3; //ondalikli olabilir
+
+            lblOrtalama.Text = sonuc.ToString();
+        }
+    }
+}
